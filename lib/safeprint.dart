f1583497007fcd949c38c6732064afc36a0557da
muhammadsaddamnur <muhammadsaddamnur@gmail.com>
2021-03-10 15:35:26 +0700

    null safety

--- lib/safeprint.dart
+++ lib/safeprint.dart
@@ -4,7 +4,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-bool isDev = false;
+bool? isDev = false;
 
 class SafePrint {
   ///Init method for setting environment [true] for Dev, [false] for Prod.
@@ -16,7 +16,7 @@ class SafePrint {
 
   ///This alternatif method for [log] on dart
   static safeLog(value, {bool time = true}) {
-    if (isDev) {
+    if (isDev!) {
       String pre = "\u001b[32m[SafeLog Dude]";
       String debugTime = !time
           ? " 😎👉"
@@ -28,7 +28,7 @@ class SafePrint {
 
   ///This alternatif method for [debugPrint] on dart
   static safeDebug(value, {bool time = true}) {
-    if (isDev) {
+    if (isDev!) {
       String pre = "\u001b[32m[SafeDebug Dude]";
       String debugTime = !time
           ? " 😎👉"
@@ -40,7 +40,7 @@ class SafePrint {
 
   ///This alternatif method for [print] on dart
   static safePrint(value, {bool time = true}) {
-    if (isDev) {
+    if (isDev!) {
       String pre = "\u001b[32m[SafePrint Dude]";
       String debugTime = !time
           ? " 😎👉"
